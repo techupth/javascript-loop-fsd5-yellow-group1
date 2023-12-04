063f5253f7445323aae375f682d2687f00563ff2
พุฒิพงศ์ วราศรีทรัพย์ <kitsune@Kitsus-MacBook.local>
2023-12-04 08:23:59 +0700

    Revert "assignments"
    This reverts commit 0eed5b5695c4fe35b684587a38cb3969422f42d6.

--- ex-6.js
+++ ex-6.js
@@ -3,7 +3,5 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
-for(let i = 0; i < companyName.length ; i++){
-reversedCompanyName = companyName[i] + reversedCompanyName;
-}
+
 console.log(reversedCompanyName);
